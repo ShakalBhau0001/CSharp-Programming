171c398547f40d719eeed4b368786f6e21fa8f10
Shakal Bhau <160219957+ShakalBhau0001@users.noreply.github.com>
2025-10-05 17:31:38 +0530

    MaxBetTwo.cs

---
+++ Basics/MaxBetTwo.cs
@@ -0,0 +1,23 @@
+namespace MaxBetTwo
+  {
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter First Number: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Second Number: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            if (n1 == n2)
+            {
+                Console.WriteLine("Both Numbers Are Equal");
+            }
+            else
+            {
+                int max = (n1 > n2) ? n1 : n2;
+                Console.WriteLine("Maximum Number: " + max);
+            }
+          Console.ReadLine();
+        }
+    }
+}
