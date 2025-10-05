2db7b7a46708ae445b160256fa6f75ded0339d39
Shakal Bhau <160219957+ShakalBhau0001@users.noreply.github.com>
2025-10-05 23:41:37 +0530

    FibonacciSeries.cs

---
+++ Basics/FibonacciSeries.cs
@@ -0,0 +1,20 @@
+namespace FibonacciSeries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int a = 0, b = 1, c;
+            Console.Write("Fibonacci Series: " + a + " " + b + " ");
+            for (int i = 2; i < n; i++)
+            {
+                c = a + b;
+                Console.Write(c + " ");
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
