bc5ea52b73f7e11d386a8e75fe711f7510ea1985
Shakal Bhau <160219957+ShakalBhau0001@users.noreply.github.com>
2025-10-08 19:19:57 +0530

    OneToTen.cs

---
+++ Basics/OneToTen.cs
@@ -0,0 +1,15 @@
+namespace OneToTen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numbers from 1 to 10:");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+          Console.ReadLine();
+        }
+    }
+}
