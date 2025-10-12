23a0830d7699ab53f6787f4f7729637ca8a9436e
Shakal Bhau <160219957+ShakalBhau0001@users.noreply.github.com>
2025-10-12 16:41:25 +0530

    SingleInheritance.cs

---
+++ OOP/SingleInheritance.cs
@@ -0,0 +1,27 @@
+namespace SingleInheritance
+{
+    internal class A
+    {
+        public void show()
+        {
+            Console.WriteLine("I AM Class A...");
+        }
+    }
+    internal class B : A
+    {
+        public void display()
+        {
+            Console.WriteLine("I AM Derived Class B...");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Single Inheritance:- ");
+            B a = new B();
+            a.show();
+            a.display();
+        }
+    }
+}
