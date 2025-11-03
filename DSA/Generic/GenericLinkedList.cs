a98cd007ca938c4ecf677aeada630ca3a486b776
Shakal Bhau <160219957+ShakalBhau0001@users.noreply.github.com>
2025-11-03 17:06:43 +0530

    Create GenericLinkedList.cs

---
+++ DSA/Generic/GenericLinkedList.cs
@@ -0,0 +1,24 @@
+namespace GenericLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> l = new LinkedList<int>();
+            // Adds at the end
+            l.AddLast(3);
+            // Adds at the beginning
+            l.AddFirst(5);
+            // Adds at the end
+            l.AddLast(7);
+            // Adds at the end
+            l.AddLast(0);
+            // Display the elements in the LinkedList
+            Console.WriteLine("Elements in the LinkedList:");
+            foreach (var i in l)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
